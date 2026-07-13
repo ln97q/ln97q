1c142513ca9dc31846f967dee029a63ad0c57d66
ln97q <hkd64zzy2h@privaterelay.appleid.com>
2026-07-14 07:41:48 +0900

    Create characters.js

---
+++ characters.js
@@ -0,0 +1,23 @@
+// キャラクターのデータをここにまとめて管理します
+const characterData = [
+  {
+    name: "キャラクターA",
+    age: "高校1年",
+    details: "16歳 / 189cm",
+    quote: "そろそろ付き合う？",
+    tags: ["オレ", "きみ"],
+    statusOk: ["甘いもの", "読書"],
+    statusNg: ["辛いもの", "人混み"],
+    expressions: ["笑顔", "イタズラ顔"]
+  },
+  {
+    name: "キャラクターB",
+    age: "高校1年",
+    details: "16歳 / 178cm",
+    quote: "ねえわ！ダボ！！",
+    tags: ["オレ", "おまえ"],
+    statusOk: ["ゲーム", "スポーツ"],
+    statusNg: ["勉強", "お化け"],
+    expressions: ["怒り", "照れ", "呆れ"]
+  }
+];
